51086d799a9f273bc67d1021581bfe449d163348
homonculus <homonculus@homonculuss-Laptop.local>
2024-02-15 00:12:49 +0545

    list

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,107 +1,168 @@
-import 'package:country_picker/country_picker.dart';
+import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Demo for country picker package',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      supportedLocales: [
-        const Locale('en'),
-        const Locale('ar'),
-        const Locale('es'),
-        const Locale('de'),
-        const Locale('fr'),
-        const Locale('el'),
-        const Locale('et'),
-        const Locale('nb'),
-        const Locale('nn'),
-        const Locale('pl'),
-        const Locale('pt'),
-        const Locale('ru'),
-        const Locale('hi'),
-        const Locale('ne'),
-        const Locale('uk'),
-        const Locale('hr'),
-        const Locale('tr'),
-        const Locale('lv'),
-        const Locale('lt'),
-        const Locale('ku'),
-        const Locale('nl'),
-        const Locale('it'),
-        const Locale.fromSubtags(
-            languageCode: 'zh',
-            scriptCode: 'Hans'), // Generic Simplified Chinese 'zh_Hans'
-        const Locale.fromSubtags(
-            languageCode: 'zh',
-            scriptCode: 'Hant'), // Generic traditional Chinese 'zh_Hant'
+      supportedLocales: const [
+        Locale("af"),
+        Locale("am"),
+        Locale("ar"),
+        Locale("az"),
+        Locale("be"),
+        Locale("bg"),
+        Locale("bn"),
+        Locale("bs"),
+        Locale("ca"),
+        Locale("cs"),
+        Locale("da"),
+        Locale("de"),
+        Locale("el"),
+        Locale("en"),
+        Locale("es"),
+        Locale("et"),
+        Locale("fa"),
+        Locale("fi"),
+        Locale("fr"),
+        Locale("gl"),
+        Locale("ha"),
+        Locale("he"),
+        Locale("hi"),
+        Locale("hr"),
+        Locale("hu"),
+        Locale("hy"),
+        Locale("id"),
+        Locale("is"),
+        Locale("it"),
+        Locale("ja"),
+        Locale("ka"),
+        Locale("kk"),
+        Locale("km"),
+        Locale("ko"),
+        Locale("ku"),
+        Locale("ky"),
+        Locale("lt"),
+        Locale("lv"),
+        Locale("mk"),
+        Locale("ml"),
+        Locale("mn"),
+        Locale("ms"),
+        Locale("nb"),
+        Locale("nl"),
+        Locale("nn"),
+        Locale("no"),
+        Locale("pl"),
+        Locale("ps"),
+        Locale("pt"),
+        Locale("ro"),
+        Locale("ru"),
+        Locale("sd"),
+        Locale("sk"),
+        Locale("sl"),
+        Locale("so"),
+        Locale("sq"),
+        Locale("sr"),
+        Locale("sv"),
+        Locale("ta"),
+        Locale("tg"),
+        Locale("th"),
+        Locale("tk"),
+        Locale("tr"),
+        Locale("tt"),
+        Locale("uk"),
+        Locale("ug"),
+        Locale("ur"),
+        Locale("uz"),
+        Locale("vi"),
+        Locale("zh")
       ],
-      localizationsDelegates: [
+      localizationsDelegates: const [
         CountryLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
       ],
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Demo for country picker')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            showCountryPicker(
-              onNotAvailableSelect: () {},
-              context: context,
-              //Optional.  Can be used to exclude(remove) one ore more country from the countries list (optional).
-              exclude: <String>['KN', 'MF'],
-              favorite: <String>['SE'],
-              //Optional. Shows phone code before the country name.
-              showPhoneCode: true,
-              onSelect: (Country country) {
-                print('Select country: ${country.displayName}');
-              },
-              // Optional. Sets the theme for the country list picker.
-              countryListTheme: CountryListThemeData(
-                // Optional. Sets the border radius for the bottomsheet.
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40.0),
-                  topRight: Radius.circular(40.0),
-                ),
-                // Optional. Styles the search field.
-                inputDecoration: InputDecoration(
-                  labelText: 'Search',
-                  hintText: 'Start typing to search',
-                  prefixIcon: const Icon(Icons.search),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: const Color(0xFF8C98A8).withOpacity(0.2),
-                    ),
-                  ),
-                ),
-                // Optional. Styles the text in the search field
-                searchTextStyle: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 18,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('CountryPicker Example'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              CountryCodePicker(
+                onChanged: print,
+                // Initial selection and favorite can be one of code ('IT') OR dial_code('+39')
+                initialSelection: 'IT',
+                favorite: const ['+39', 'FR'],
+                countryFilter: const ['IT', 'FR'],
+                showFlagDialog: false,
+                comparator: (a, b) => b.name.compareTo(a.name),
+                //Get the country information relevant to the initial selection
+                onInit: (code) => debugPrint(
+                    "on init ${code.name} ${code.dialCode} ${code.name}"),
+              ),
+              CountryCodePicker(
+                onChanged: print,
+                // Initial selection and favorite can be one of code ('IT') OR dial_code('+39')
+                initialSelection: 'IT',
+                favorite: const ['+39', 'FR'],
+                countryFilter: const ['IT', 'FR'],
+                // flag can be styled with BoxDecoration's `borderRadius` and `shape` fields
+                flagDecoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(7),
                 ),
               ),
-            );
-          },
-          child: const Text('Show country picker'),
+              const SizedBox(
+                width: 400,
+                height: 60,
+                child: CountryCodePicker(
+                  onChanged: print,
+                  hideMainText: true,
+                  showFlagMain: true,
+                  showFlag: false,
+                  initialSelection: 'TF',
+                  hideSearch: true,
+                  showCountryOnly: true,
+                  showOnlyCountryWhenClosed: true,
+                  alignLeft: true,
+                ),
+              ),
+              SizedBox(
+                width: 400,
+                height: 60,
+                child: CountryCodePicker(
+                  onChanged: (element) => debugPrint(element.toLongString()),
+                  initialSelection: 'TF',
+                  showCountryOnly: true,
+                  showOnlyCountryWhenClosed: true,
+                  favorite: const ['+39', 'FR'],
+                ),
+              ),
+              SizedBox(
+                width: 100,
+                height: 60,
+                child: CountryCodePicker(
+                  enabled: false,
+                  onChanged: (c) => c.name,
+                  initialSelection: 'TF',
+                  showCountryOnly: true,
+                  showOnlyCountryWhenClosed: true,
+                  favorite: const ['+39', 'FR'],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
